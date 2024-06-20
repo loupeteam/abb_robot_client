048cc7c63ef048b562d3a48582df0ba774ba93c8
agrayzel <ari@loupe.team>
2024-06-20 17:37:22 -0400

    Add script to install locally in IsaacSim

---
+++ buildAndCopy.bat
@@ -0,0 +1,3 @@
+cp -r . C:\\Users\\%USERNAME%\\Appdata\\Local\\ov\\pkg\\isaac_sim-2023.1.1\\abb_robot_client
+
+C:\\Users\\%USERNAME%\\Appdata\\Local\\ov\\pkg\\isaac_sim-2023.1.1\\python.bat C:\\Users\\%USERNAME%\\Appdata\\Local\\ov\\pkg\\isaac_sim-2023.1.1\\abb_robot_client\\setup.py install
